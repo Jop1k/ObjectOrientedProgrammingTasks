785e79b0ba44a33da77b9f81ce01a4a987bde1c6
Jop1k <cgggeghhg@gmail.com>
2025-04-30 16:50:29 +0800

    feat: added method "Validator"

--- Task-1/Program.cs
+++ Task-1/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main()
     {
+        Console.WriteLine("Введите количество создаваемых вами Person:");
+        int count = Validator(Console.ReadLine()!);
 
+    }
+    static int Validator(string str)
+    {
+        while (true)
+        {
+            int number;
+
+            if (int.TryParse(str, out number))
+            {
+                return number;
+            }
+
+            Console.WriteLine("Введите число или цифру:");
+            str = Console.ReadLine()!;
+        }
     }
 }
