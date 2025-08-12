3b3247cca4ecfd67dea5e4642221ef2fa182aeb2
Jop1k <cgggeghhg@gmail.com>
2025-08-12 19:54:26 +0800

    feat: added unit-tests for class ProductInfo

--- StoreManagement.Tests/ProductInfoTests.cs
+++ StoreManagement.Tests/ProductInfoTests.cs
@@ -2,5 +2,21 @@
 
 public class ProductInfoTests
 {
+    [Fact]
+    public void Quantity_SetNegativeValue_ThrowsException()
+    {
+        var product = new Product("card", 0);
+        var productInfo = new ProductInfo(product, 100, 10);
 
+        Assert.Throws<InvalidQuantityException>(() => productInfo.Quantity = -5);
+    }
+
+    [Fact]
+    public void Price_SetNegativeValue_ThrowsException()
+    {
+        var product = new Product("book", 0);
+        var productInfo = new ProductInfo(product, 100, 10);
+
+        Assert.Throws<InvalidPriceException>(() => productInfo.Price = -5);
+    }
 }
